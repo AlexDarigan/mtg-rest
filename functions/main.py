629ee5fcff26ebab2b726825c3005ae15447a6ae
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-11 10:23:01 +0000

    add transformation plan

--- functions/main.py
+++ functions/main.py
@@ -1,7 +1,3 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
@@ -11,3 +7,17 @@ from firebase_admin import initialize_app
 # @https_fn.on_request()
 # def on_request_example(req: https_fn.Request) -> https_fn.Response:
 #     return https_fn.Response("Hello world!")
+
+
+# Service: Gatherer
+# Service: Preprocessor
+# Service: Publisher
+# Service: Firestore
+# Service: DataExplorer (? <--- ) / API thing
+
+# process()
+# publish()
+
+
+
+
