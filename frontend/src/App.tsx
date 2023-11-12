26d2b6257d785f4e5d4aaa18b519e31077d14756
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-12 18:32:43 +0000

    Add successful frontend request from live app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+  // Callback Reference
+  const fetcher = useCallback(async () => {
+    var response = await fetch("https://run.mocky.io/v3/bb21935b-b670-48f8-bf0f-a50d28328ba2")
+    var data = await response.json()
+    setText(JSON.stringify(data))
+  }, [])
+
+  // 
+  const fetcher2 = useCallback(async () => {
+    // Error was using no cors && also bad url (check for bad plurals, wrong-order)
+    var response = await fetch("https://mtg-rest.web.app/api/v1/measure/cardtypes")
+    console.log(response)
+    var data = await response.json()
+    console.log(data)
+  }, [])
 
   return (
     <>
@@ -21,6 +38,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => fetcher()}>Text is {text}</button>
+        <button onClick={() => fetcher2()}>App {text}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
