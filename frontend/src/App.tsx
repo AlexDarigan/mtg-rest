8b063322abcdd291066190d2b02e87df93efb23d
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-15 10:24:27 +0000

    remove default css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Plot from 'react-plotly.js';
-import './App.css'
+
+
+function NavBar() {
+  return (
+    <nav className='nav'>
+      <a href="" className="site-title">Site Name</a>
+      <ul>
+        <li>
+          <a href="/github">Github</a>
+        </li>
+        <li>
+          <a href="/linkedin"></a>
+        </li>
+        <li>
+          <a href="/email"></a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,46 +53,41 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"/>
-      <img src={img} style={{border: "2px black solid"}}/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => fetcher()}>Text is {text}</button>
-        <button onClick={() => fetcher2()}>App {text}</button>
-        <button onClick={() => trends()}>Get Trends</button>
-        <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <NavBar></NavBar>
+    </div>
     </>
   )
 }
 
 export default App
+
+
+// <img src={img} style={{border: "2px black solid"}}/>
+// <div className="card">
+//   <button onClick={() => setCount((count) => count + 1)}>
+//     count is {count}
+//   </button>
+//   <button onClick={() => fetcher()}>Text is {text}</button>
+//   <button onClick={() => fetcher2()}>App {text}</button>
+//   <button onClick={() => trends()}>Get Trends</button>
+//   <Plot
+//   data={[
+//     {
+//       x: [1, 2, 3],
+//       y: [2, 6, 3],
+//       type: 'scatter',
+//       mode: 'lines+markers',
+//       marker: {color: 'red'},
+//     },
+//     {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+//   ]}
+//   layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+// />
+//   <p>
+//     Edit <code>src/App.tsx</code> and save to test HMR
+//   </p>
+// </div>
+// <p className="read-the-docs">
+//   Click on the Vite and React logos to learn more
+// </p>
