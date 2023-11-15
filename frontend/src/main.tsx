8290478782fce9a2b6612cd08893309ca431453c
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-15 16:59:36 +0000

    remove unused elements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-import "./styles.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './routes/root';
 import Portfolio from './routes/portfolio';
 import DSML from './routes/dsml';
