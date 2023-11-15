ba0bb62ba4c48aa5853df7ee92f365fb4e232a77
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-15 19:05:12 +0000

    add complete portfolio

--- frontend/src/routes/portfolio.tsx
+++ frontend/src/routes/portfolio.tsx
@@ -5,8 +5,8 @@ function Portfolio() {
 
   return (
     <>
-    <PanelGroup bordered>
-      <Panel header={<h3>About Me - David Darigan</h3>} style={{fontSize: "1rem"}}>
+    <PanelGroup bordered style={{fontSize: "1rem"}}>
+      <Panel bordered header={<h3>About Me - David Darigan</h3>}>
         <Row>
           <Col  xs={4}><img src={Me} height={200}></img></Col>
           <Col xs={12}>
@@ -23,9 +23,8 @@ function Portfolio() {
         </Row>
       </Panel>
       
-      <Panel header={<h3>Experience</h3>} style={{fontSize: "1rem"}}>
-        <PanelGroup>
-        <Panel header="DELL Technologies Undergraduate Internship March 2023 - August 2023">
+      <Panel bordered header={<h3>Experience</h3>}>
+        <Panel header={<h5>DELL Technologies Undergraduate Internship March 2023 - August 2023</h5>}>
           <ul>
           <li>Contributed to the efforts of the Finance Integration Services Team to support over 150 class 1 financial microservices.</li>
           <li>Used PowerShell to reduce 2 days of work a year to 30 minutes by automating the task of restaging all applications in each non-production environment.</li>
@@ -35,25 +34,52 @@ function Portfolio() {
           <li>Utilized GitLab & Spring Configuration Server REST APIs in order to write a feature in Bash to set metadata on applications during the deploy task of the deployment pipeline.</li>
           </ul>
         </Panel>
-        <Panel header="Google Developers Student Club Team Lead 2022 - Present">
+        <Panel header={<h5>Google Developers Student Club Team Lead 2022 - Present</h5>}>
           <ul>
             <li>Co-ordinate weekly events</li>
             <li>Communicate fundamental technological ideas to members through presentations</li>
             <li>Manage budgeting with the aid of Google Developer Student Club co-ordinators</li>
           </ul>
       </Panel>
-      <Panel header="Google Developers Student Club Core Team Member 2021 - 2022" >
+      <Panel header={<h5>Google Developers Student Club Core Team Member 2021 - 2022</h5>}>
         <ul>
           <li>Create applications in order to demonstrate a diverse range of technologies used in presentations</li>
           <li>Assist other core members with the organization of events</li>
         </ul>
       </Panel>
-      </PanelGroup>
-      <Panel header={<h3>Education</h3>} style={{fontSize: "1rem"}}>
-        
+      <Panel header={<h3>Education</h3>}>
+        <ul>
+          <li>BSc in Software Development (First Class Honours) 2020 - 2023</li>
+          <li>Currently studying for BSc (Honours) in Software Development 2023 - 2024</li>
+        </ul>
       </Panel>
-      <Panel header={<h3>Technologies</h3>} style={{fontSize: "1rem"}}>
-        
+      <Panel header={<h3>Technologies</h3>}>
+        <Row>
+          <Col>
+            <ul>
+              <li>Kotlin</li>
+              <li>Java</li>
+              <li>Python</li>
+              <li>C#</li>
+            </ul>
+          </Col>
+          <Col>
+            <ul>
+              <li>Spring / Spring Boot</li>
+              <li>Maven</li>
+              <li>Git</li>
+              <li>Docker</li>
+            </ul>
+          </Col>
+          <Col>
+            <ul>
+              <li>HTTP</li>
+              <li>C / C++</li>
+              <li>SQL</li>
+              <li>SQLite</li>
+            </ul>
+          </Col>
+        </Row>
       </Panel>
     </Panel>
     </PanelGroup>
