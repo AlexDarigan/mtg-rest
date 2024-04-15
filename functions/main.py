2d50ca4adb8bc7de937db22c720f595edd08a486
AlexDarigan <47497501+AlexDarigan@users.noreply.github.com>
2024-04-15 04:19:46 +0100

    add canvas and rewrite

--- functions/main.py
+++ functions/main.py
@@ -2,12 +2,17 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+app = initialize_app()
+
+
+@https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"]))
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    # number = req.data["number"]
+    # print(req.data)
+    # length = str(len(number))
+    print(req)
+    return {"message": "We got your msg chars"}
+
